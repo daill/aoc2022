71dd2f91883a233f782f63a3630f8f92d835aaa4
Christian Kramer <ck.daill@gmail.com>
2022-12-02 16:30:26 +0100

    initial commit

---
+++ 1/target/debug/deps/day1-a9731b655e2196d2.d
@@ -0,0 +1,5 @@
+/Users/daill/Devel/workspaces/aoc2022/1/target/debug/deps/day1-a9731b655e2196d2: src/main.rs
+
+/Users/daill/Devel/workspaces/aoc2022/1/target/debug/deps/day1-a9731b655e2196d2.d: src/main.rs
+
+src/main.rs:
