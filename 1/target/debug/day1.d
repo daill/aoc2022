b1aedcc863d8869836d443259d9c279021c701d9
Christian Kramer <ck.daill@gmail.com>
2022-12-02 20:40:32 +0100

    base

--- 1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-C:\Devel\workspaces\aoc2022\1\target\debug\day1.exe: C:\Devel\workspaces\aoc2022\1\src\main.rs
--- 1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-C:\Devel\workspaces\aoc2022\1\target\debug\deps\day1.exe: src\main.rs
-
-C:\Devel\workspaces\aoc2022\1\target\debug\deps\day1.d: src\main.rs
-
-src\main.rs:
