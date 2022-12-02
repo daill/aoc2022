11d4fc39d4f1985cbb5a8aaa486da2a3563c2733
Christian Kramer <ck.daill@gmail.com>
2022-12-02 20:39:08 +0100

    base

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,3 +1,30 @@
+extern crate core;
+
+use core::panicking::panic;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, Read};
+
+fn read_from_file() {
+    let mut file = File::open("input");
+    let content  = match file {
+        Ok(file) => {
+            let lines = io::BufReader::new(file).lines();
+            let mut result: Vec<String> = Vec::new();
+            for line in lines {
+                if let Ok(line) = line {
+                    result.push(line);
+                }
+            }
+        },
+        Err(e) => panic!(e)
+    };
+    return content
+}
+
+
+
 fn main() {
+    let content: () = read_from_file();
     println!("Hello, world!");
 }
---
+++ 1/target/debug/day1.d
@@ -0,0 +1,1 @@
+C:\Devel\workspaces\aoc2022\1\target\debug\day1.exe: C:\Devel\workspaces\aoc2022\1\src\main.rs
