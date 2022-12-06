14835bb2ff2c670daa2b2b19996b8d8d5fbf296f
Christian Kramer <ck.daill@gmail.com>
2022-12-06 16:28:26 +0100

    transfer

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -8,56 +9,85 @@ use regex::Regex;
 // a-z 1-26
 // A-Z 27-52
 
-fn read_from_file() -> Vec<Vec<char>> {
+fn read_from_file() -> (Vec<Vec<char>>, Vec<Vec<i32>>) {
     let mut file = File::open("input");
-    let result:Vec<Vec<char>> = match file {
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    let mut procedure: Vec<Vec<i32>> = Vec::new();
+    let (stacks, procedure) = match file {
         Ok(file) => {
             let lines = io::BufReader::new(file).lines();
             let re = Regex::new(r"\w+\W(\d+)\W\w+\W(\d+)\W\w+\W(\d+)").unwrap();
-            let mut result: Vec<Vec<char>> = Vec::new();
-            let mut procedure: Vec<Vec<i32>> = Vec::new();
             let mut procedure_part: bool = false;
             for line in lines {
                 if let Ok(line) = line {
-                    println!("{}", line);
+                    println!("line {}", line);
                     if line.is_empty() {
                         procedure_part = true;
+
                         continue;
                     }
                     if procedure_part == false {
-                        let mut line_vec: Vec<char> = Vec::new();
-                        let mut counter = 1;
+
+                        let mut counter = line.len()-2;
+                        let mut line_counter = 0;
                         while counter < line.len() {
-                            line_vec.push(line.chars().nth(counter).unwrap());
-                            counter += 4;
+                            if stacks.len() == line_counter {
+                                stacks.push(Vec::new());
+                            }
+                            let c = line.chars().nth(counter).unwrap();
+                            if c.is_alphabetic() {
+                                stacks[line_counter].push(c);
+                            }
+
+                            if counter < 4 {
+                                break;
+                            }
+                            counter -= 4;
+
+                            line_counter += 1;
                         }
-                        println!("{:?}", line_vec);
-                        result.push(line_vec);
+
                     } else {
                         for capture in re.captures_iter(&line) {
-                            print!("{}", &capture[1].parse::<i32>().unwrap())
+                            let a = i32::from(*&capture[1].parse::<i32>().unwrap());
+                            let b = i32::from(*&capture[2].parse::<i32>().unwrap())-1;
+                            let c = i32::from(*&capture[3].parse::<i32>().unwrap())-1;
+                            procedure.push(Vec::from([a,b,c]));
                         }
-                        println!("{}", line);
                     }
                 }
             }
-            result
+            stacks.reverse();
+            println!("stacks {:?}", &stacks);
+            print!("{:?}", procedure);
+            (stacks, procedure)
         },
         Err(e) => panic!("Cannot process file: {}", e)
     };
-    result
+    return (stacks, procedure);
 }
 
 
-fn first_part(content: &Vec<Vec<char>>) {
+fn first_part(stacks: &mut Vec<Vec<char>>, procedure: &Vec<Vec<i32>>) {
+    for proc in procedure {
+        let amount = &proc[0];
+        let from = &proc[1]-1;
+        let to = &proc[2]-1;
 
+
+        for a in 0..*amount {
+            let b: Vec<char> = stacks.get_mut(from).unwrap();
+        }
+
+    }
 }
 
 
 fn main() {
-    let content = read_from_file();
+    let (stacks, procedure) = read_from_file();
 
-    first_part(&content);
+
+    //first_part(&content);
 
 
 
