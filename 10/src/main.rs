45552cb7cf19f2971cdfa7f6208335fcb4e2f571
Christian Kramer <ck.daill@gmail.com>
2022-12-12 13:42:45 +0100

    transfer

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -9,18 +9,18 @@ use std::str::Chars;
 
 
 
-fn read_from_file() -> Vec<(String, usize)> {
+fn read_from_file() -> Vec<(String, i32)> {
     let mut file = File::open("input");
-    let mut content: Vec<(String, usize)> = Vec::new();
+    let mut content: Vec<(String, i32)> = Vec::new();
     let content = match file {
         Ok(file) => {
             let lines = io::BufReader::new(file).lines();
             for line in lines {
 
                 if let Ok(line) = line {
-                    println!("line {}", line);
-                    let (a, b) = line.split_at(2);
-                    content.push((String::from(a.trim()), b.parse::<usize>().unwrap_or(0)));
+                    let (a, b) = line.split_at(4);
+                    println!("line {} a {:?} b {:?}", line, a,b.trim());
+                    content.push((String::from(a), b.trim().parse::<i32>().unwrap_or(0)));
 
                 }
             }
@@ -35,25 +35,33 @@ fn read_from_file() -> Vec<(String, usize)> {
 
 
 
-fn first_part(content: &Vec<(String, usize)>)  {
+fn first_part(content: &Vec<(String, i32)>)  {
     let mut cycle = 0;
-    let mut x = 0;
+    let mut x = 1;
+    let mut sum = 0;
     for cmd in content {
-        match cmd.0.as_str() {
-            "noop" => {
-                add_cycle(&mut cycle, 1);
-            },
-            "addx" => {
-                add_cycle(&mut cycle, 2);
-                x += cmd.1;
-            },
-            _ => {}
+        for i in 0..process_cmd(&cmd.0) {
+            cycle += 1;
+            match cycle {
+                20 | 60 | 100 | 140 | 180 | 220 => {
+                    let tmp = x*cycle;
+                    println!("=> {} {} {}", x, cycle, tmp);
+                    sum += tmp;
+                },
+                _ => {}
+            }
         }
+        x += cmd.1;
     }
+    println!("{}", sum);
 }
 
-fn add_cycle(mut cycle: &mut i32, add: usize) {
-
+fn process_cmd(cmd: &String) -> i32 {
+    match cmd.as_str() {
+        "noop" => 1,
+        "addx" => 2,
+        _ => 0
+    }
 }
 
 
@@ -62,8 +70,6 @@ fn main() {
     println!("content {:?}", &content);
 
     first_part(&content);
-
-
 
 
 }
