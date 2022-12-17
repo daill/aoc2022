d3827ebb695a84f108c5fcc73d99c01017390013
Christian Kramer <ck.daill@gmail.com>
2022-12-17 12:06:30 +0100

    transfer

--- 12/src/main.rs
+++ 12/src/main.rs
@@ -8,7 +8,6 @@ use std::slice::RSplit;
 use std::str::Chars;
 use std::task::ready;
 
-
 fn read_from_file() -> (u32, Vec<char>, Vec<(u32, (u32, u32))>) {
     let mut file = File::open("input");
     let mut content: Vec<char> = Vec::new();
@@ -23,8 +22,8 @@ fn read_from_file() -> (u32, Vec<char>, Vec<(u32, (u32, u32))>) {
                     if length == 0 {
                         length = line.len() as u32;
                     }
-                    content.append(&mut line.chars().enumerate().map(|i, c| {
-                        if c != &"S" {
+                    content.append(&mut line.chars().enumerate().map(|(i, c)| {
+                        if c != 'S' {
                             dists.push((u32::MAX, (i.clone() as u32, id.clone() as u32)));
                         }
                         c
@@ -43,25 +42,38 @@ fn get_lin_pos(length: &u32, position: &(u32, u32)) -> usize {
     (length * position.1 + position.0) as usize
 }
 
-fn check_char_at(content: &Vec<char>, current_char: &char, length: &u32, position: &(u32, u32), dists: &mut Vec<(u32, (u32, u32))>) {
+fn check_char_at(content: &Vec<char>, current_char: &char, length: &u32, cur_pos: &(u32, u32), position: &(u32, u32), dists: &mut Vec<(u32, (u32, u32))>) {
 
     let mut char_at : &char = &char::default();
     char_at = content.get(get_lin_pos(length, position)).unwrap();
 
-    if (*current_char as u32) >= (*char_at as u32) {
-        let id = dists.iter().position(|e| e.1 == *position).unwrap();
-        dists.insert(id, (position), 0);
+    let mut val = ((*current_char as u32)+1);
+    let mut dist = 0;
+    if current_char == &'S' {
+        val = ('a' as u32) + 1;
+        dist = 1;
+    } else {
+        if let Some(cur) =  dists.iter().position(|e| e.1 == *cur_pos) {
+            dist = (dists.get(cur).unwrap().0) + 1;
+            dists.remove(cur);
+        }
     }
+    if val >= (*char_at as u32) {
+        if let Some(id) = dists.iter().position(|e| e.1 == *position) {
+            dists.get_mut(id).unwrap().0 = u32::min(dists.get(id).unwrap().0, dist);
+        }
+    }
+    println!("{:?}", dists);
 
 }
 
 fn walk_path(content: &Vec<char>, visited: &Vec<(u32, u32)>, length: &u32, position: (u32, u32), paths: &mut HashMap<u32, Vec<(u32, u32)>>, dists: &mut Vec<(u32, (u32, u32))>) {
     let mut current_char = &char::default();
-    let mut next_pos = position;
+    let mut cur_pos = position;
     let mut vis: Vec<(u32, u32)> = Vec::new();
     let mut ds: Vec<(i32, (u32, u32))> = Vec::new();
     loop {
-        current_char = content.get(get_lin_pos(length, &next_pos)).unwrap();
+        current_char = content.get(get_lin_pos(length, &cur_pos)).unwrap();
         //println!("cc {} pos {:?}", current_char, position);
         if current_char == &'E' {
             //println!("{} {:?}", local_visited.len(), local_visited);
@@ -69,36 +81,35 @@ fn walk_path(content: &Vec<char>, visited: &Vec<(u32, u32)>, length: &u32, posit
         }
 
 
-        if next_pos.0 < *length - 1 {
-            let p_r = (next_pos.0 + 1, next_pos.1);
+        if cur_pos.0 < *length - 1 {
+            let p_r = (cur_pos.0 + 1, cur_pos.1);
             if !vis.contains(&p_r) {
-                ds.push((check_char_at(content, current_char, length, &p_r, dists), p_r));
+               check_char_at(content, current_char, length, &cur_pos, &p_r,  dists);
             }
         }
-        if next_pos.1 < (content.len() as u32) / length - 1 {
-            let p_u = (next_pos.0, next_pos.1 + 1);
+        if cur_pos.1 < (content.len() as u32) / length - 1 {
+            let p_u = (cur_pos.0, cur_pos.1 + 1);
             if !vis.contains(&p_u) {
-                ds.push((check_char_at(content, current_char, length, &p_u, dists), p_u));
+                check_char_at(content, current_char, length,&cur_pos,  &p_u, dists);
             }
         }
-        if next_pos.1 > 0 {
-            let p_d = (next_pos.0, next_pos.1 - 1);
+        if cur_pos.1 > 0 {
+            let p_d = (cur_pos.0, cur_pos.1 - 1);
             if !vis.contains(&p_d) {
-                ds.push((check_char_at(content, current_char, length, &p_d, dists), p_d));
+                check_char_at(content, current_char, length, &cur_pos, &p_d, dists);
             }
         }
-        if next_pos.0 > 0 {
-            let p_l = (next_pos.0 - 1, next_pos.1);
+        if cur_pos.0 > 0 {
+            let p_l = (cur_pos.0 - 1, cur_pos.1);
 
             if !vis.contains(&p_l) {
-                ds.push((check_char_at(content, current_char, length, &p_l, dists), p_l));
+                check_char_at(content, current_char, length, &cur_pos, &p_l, dists);
             }
 
         }
 
-        vis.push(next_pos);
-        next_pos = ds.iter().min_by(|a, b| a.0.cmp(&b.0) ).map(|a| a.1).unwrap().clone();
-        ds.clear();
+        vis.push(cur_pos);
+        cur_pos = dists.iter().min_by(|a, b| a.0.cmp(&b.0) ).map(|e| e.1 ).unwrap();
     }
 
 }
