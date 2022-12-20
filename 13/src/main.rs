4d7a21d7f9d82e07c5fb4e48c234b050c93cf709
Christian Kramer <ck.daill@gmail.com>
2022-12-20 12:08:10 +0100

    transfer

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -9,85 +9,53 @@ use std::rc::Rc;
 use std::slice::RSplit;
 use std::str::Chars;
 
-#[derive(Debug)]
-enum TreeType {
-    Value,
-    Branch,
+#[derive(Debug, Clone)]
+enum Token {
+    Number(u32),
+    List(Box<Vec<Token>>)
 }
 
-#[derive(Debug)]
-struct Tree {
-    tree_type: TreeType,
-    parent: Option<Rc<Tree>>,
-    left: Option<Rc<Tree>>,
-    right: Option<Rc<Tree>>,
-    values: Vec<u32>,
-}
-
-impl Tree {
-    fn new_branch() -> Tree {
-        Tree {
-            tree_type: TreeType::Branch,
-            parent: None,
-            left: None,
-            right: None,
-            values: vec![],
-        }
-    }
-
-    fn new_value() -> Tree {
-        Tree {
-            tree_type: TreeType::Value,
-            parent: None,
-            left: None,
-            right: None,
-            values: vec![],
-        }
-    }
-
-    fn add_child(&mut self, child: Tree) {
-        if self.left.is_none() {
-            self.left = Option::from(Rc::new(child));
-        } else {
-            self.right = Option::from(Rc::new(child));
-        }
-    }
-}
-
-fn read_from_file() -> Vec<Tree> {
+fn read_from_file() -> Vec<Token> {
     let mut file = File::open("input");
-    let mut content: Vec<Tree> = Vec::new();
+    let mut content: Vec<Token> = Vec::new();
     let content = match file {
         Ok(file) => {
             let lines = io::BufReader::new(file).lines();
             for line in lines {
-
+                let mut stack = vec![vec![]];
+                let mut element = String::default();
+                let mut idx = 0;
                 if let Ok(line) = line {
                     if line.is_empty() {
                         continue;
                     }
-                    let mut root = Tree::new_branch();
-                    let mut current = &mut root;
-                    content.push(root);
-
                     let mut element = String::default();
 
                     line.chars().skip(1).for_each(|e| {
                         match e {
                             '[' => {
-                                let mut a = Tree::new_branch();
-                                current.add_child(a);
-                                current = &mut a;
+                                stack.push(vec![]);
+                                idx += 1;
                             },
-                            ','|']' => {
-
+                            ',' => {
+                                if element != String::default() {
+                                    stack.get_mut(idx).unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
+                                    element = String::default();
+                                }
                             },
+                            ']' => {
+                                if element != String::default() {
+                                    stack.get_mut(idx).unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
+                                    element = String::default();
+                                }
+                                idx -= 1;
+                            }
                             _ => {element.push(e)}
                         }
                     });
                 }
+                println!("content {:?}", stack);
             }
-            println!("content {:?}", content);
             content
         },
         Err(e) => panic!("Cannot process file: {}", e)
@@ -99,14 +67,14 @@ fn read_token(chars: &Vec<char>, start: usize) {
 
 }
 
-fn first_part(content: &Vec<Tree>)  {
+fn first_part(content: &Vec<Token>)  {
 
 
 }
 
 
 fn main() {
-    let content: Vec<Tree> = read_from_file();
+    let content: Vec<Token> = read_from_file();
     println!("content {:?}", &content);
 
 
