5d17a6466d2b6a185fd2a472275ea1340a972168
Christian Kramer <ck.daill@gmail.com>
2022-12-21 10:20:34 +0100

    transfer

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -16,6 +16,8 @@ enum Token {
     List(Box<Vec<Token>>)
 }
 
+
+
 fn read_from_file() -> Vec<Token> {
     let mut file = File::open("input");
     let mut content: Vec<Token> = Vec::new();
@@ -25,6 +27,7 @@ fn read_from_file() -> Vec<Token> {
             for line in lines {
                 let mut stack = vec![vec![]];
                 let mut element = String::default();
+                let idx = stack.len()-1;
                 if let Ok(line) = line {
                     if line.is_empty() {
                         continue;
@@ -38,27 +41,24 @@ fn read_from_file() -> Vec<Token> {
                             },
                             ',' => {
                                 if element != String::default() {
-                                    let idx = stack.len()-1;
-                                    stack.get_mut(idx).unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
+                                    stack.last_mut().unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
                                     element = String::default();
                                 }
                             },
                             ']' => {
                                 if element != String::default() {
-                                    let idx = stack.len()-1;
-                                    stack.get_mut(idx).unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
+                                    stack.last_mut().unwrap().push(Token::Number(element.parse::<u32>().unwrap()));
                                     element = String::default();
                                 }
                                 let v = stack.pop().unwrap();
-                                let idx = stack.len()-1;
-                                stack.get_mut(idx).unwrap().push(Token::List(Box::from(v)));
+                                stack.last_mut().unwrap().push(Token::List(Box::from(v)));
                             }
                             _ => {element.push(e)}
                         }
                     });
                 }
                 println!("content {:?}", stack);
-                content.p
+                content.push(Token::List(Box::from((stack.pop().unwrap()))));
             }
             content
         },
@@ -67,22 +67,63 @@ fn read_from_file() -> Vec<Token> {
     return content
 }
 
-fn read_token(chars: &Vec<char>, start: usize) {
-
-}
-
-fn first_part(content: &Vec<Token>)  {
 
 
+fn cmp(left: &Token, right: &Token) -> bool {
+    match (left, right) {
+        (Token::Number(left_value), Token::Number(right_value)) => {
+            println!("{} {}", left_value, right_value);
+            if left_value > right_value {
+                return false;
+            }
+            true
+        },
+        (Token::Number(left_value), Token::List(right_value)) => {
+            cmp(&Token::List(Box::from(vec![Token::Number(left_value.clone())])), right)
+        },
+        (Token::List(left_value), Token::Number(right_value)) => {
+            cmp(left, &Token::List(Box::from(vec![Token::Number(right_value.clone())])))
+        }
+        (Token::List(left_value), Token::List(right_value)) => {
+            let mut i = 0;
+            let mut ret = false;
+            loop {
+                if i < left_value.len() && i < right_value.len() {
+                    if let (Some(l_token), Some(r_token)) = (left_value.get(i), right_value.get(i)) {
+                        ret = cmp(l_token, r_token);
+                        if ret == false {
+                            break;
+                        }
+                    }
+                } else if i >= left_value.len() && i < right_value.len(){
+                    ret = true;
+                    break;
+                } else {
+                    break;
+                }
+                i += 1;
+
+            }
+            ret
+        }
+        _ => false
+    }
 }
 
 
 fn main() {
     let content: Vec<Token> = read_from_file();
+
+    for i in (0..content.len()).step_by(2) {
+        let left = content.get(i).unwrap();
+        let right = content.get(i+1).unwrap();
+
+        println!("{}", cmp(left, right));
+    }
+
     println!("content {:?}", &content);
 
 
-    first_part(&content);
 
 
 
